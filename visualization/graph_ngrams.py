1228c6736421421a5fe507c9f309763d0d18dbf2
mayachamberlain <79328636+mayachamberlain@users.noreply.github.com>
2021-03-15 14:09:54 -0500

    Update graph_ngrams.py
    Added comments

--- visualization/graph_ngrams.py
+++ visualization/graph_ngrams.py
@@ -6,28 +6,35 @@ Graphs the three n-grams plots (but doesn't save rn)
 """
 
 def make_bar(df, key, end):
+    """
+    Creates horizontal bar plot formatted for our three n-grams plots.
+    Inputs are: dataset, part of data used, number of most common n-grams plotted. 
+    """
+    # Selecting data to be plotted
     df = df[:end]
     plt.figure(figsize=(10, 7))
     plt.barh(range(df.shape[0]), df['counts'])
     plt.ticklabel_format(style='plain')
-
+    
     plt.yticks(range(df.shape[0]), df[key])
     plt.gca().invert_yaxis()
-
+    
+    # Creating labels/titles.
     plt.xlabel('Number of Appearances')
     plt.title('25 Most Common Terms in Covid-Related Tweets')
     plt.show()
 
-
+# Creating variables for csv files
 terms_folder = "../data-collection/data/all_term_counts_clean.csv"
 bigrams_folder = "../data-collection/data/all_bigram_counts_clean.csv"
 trigrams_folder = "../data-collection/data/all_trigram_counts_clean.csv"
 
-
+# Reading in csv files.
 o_df = pd.read_csv(terms_folder)
 b_df = pd.read_csv(bigrams_folder)
 t_df = pd.read_csv(trigrams_folder)
 
+# Creating horizontal bar plots of previous csv files.
 make_bar(o_df, 'term', 25)
 
 make_bar(b_df, 'gram', 25)
