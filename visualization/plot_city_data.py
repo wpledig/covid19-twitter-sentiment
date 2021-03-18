6dbb0c8d9f1202c1187042f9cf414534e2b63d0f
DBelsh <dab12@rice.edu>
2021-03-17 19:13:06 -0500

    Commented plot_city_data

--- visualization/plot_city_data.py
+++ visualization/plot_city_data.py
@@ -9,20 +9,24 @@ Plots tweets per city
 
 input_file = "../exploration/location_data.csv"
 
+# initialize pandas dataframes for visualization
 df = pd.read_csv(input_file)
 df = df[df["lat"] != 0.0]
 cities = df[df["place_type"] == 'city']
 cities = cities[:2500]
 
+# initialize matplotlib basemap
 m = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49, projection='lcc',lat_1=33,lat_2=45,lon_0=-95)
 
 # load the shapefile, use the name 'states'
 m.readshapefile('st99_d00', name='states', drawbounds=True)
 
+# normalize distribution of volume of tweets per city
 vmin = cities["count"].min()
 vmax = cities["count"].max()
 norm = Normalize(vmin=vmin, vmax=vmax)
 
+# convert geographic data to Cartesian coordinates
 x, y = m(cities["lng"], cities["lat"])
 
 m.scatter(x, y, s=cities["count"] / 50.0,  alpha=0.5)
