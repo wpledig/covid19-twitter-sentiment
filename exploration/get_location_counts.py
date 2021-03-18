6924dab3ca95366a3c0de16ae59409178bc45a9b
DBelsh <dab12@rice.edu>
2021-03-17 19:24:57 -0500

    Commented get_location_counts

--- exploration/get_location_counts.py
+++ exploration/get_location_counts.py
@@ -12,32 +12,33 @@ dataset = "../data-collection/data/complete_en_US"
 
 num_counted = 0
 
+# generate dictionary which stores location data from 'dataset'
 with open(dataset) as inputfile:
     lines = inputfile.readlines()
     print("filesize: ", len(lines))
-    for line in lines:
-        jline = json.loads(line)
-        if jline['place'] is None:
+    for line in lines: # for each line
+        jline = json.loads(line) # interpret each line as a json entry
+        if jline['place'] is None: # skip if no place listed
             continue
         location_id = jline['place']['id']
         place = jline['place']
-        if location_id not in location_counts.keys():
-            location_counts[location_id] = {
+        if location_id not in location_counts.keys(): # if location has not been encountered yet, create new entry
+            location_counts[location_id] = { # copy json information into a python dictionary
                 'name': place['name'],
                 'full_name': place['full_name'],
                 'place_type': place['place_type'],
                 'count': 0
             }
 
-        location_counts[location_id]['count'] += 1
+        location_counts[location_id]['count'] += 1 # add 1 to count within dictionary
 
-        num_counted += 1
-        if num_counted % 1000 == 0:
+        num_counted += 1 # add 1 to total number of locations counted
+        if num_counted % 1000 == 0: # every 1000 locations counted, print progress
             print("Counted ", num_counted)
 
-df = pd.DataFrame.from_dict(location_counts, orient='index').reset_index()
+df = pd.DataFrame.from_dict(location_counts, orient='index').reset_index() # generate pandas dataframe from dictionary
 # df.columns = ['location_id', 'count']
 print(df)
 df = df.sort_values(by='count', ascending=False)
-df.to_csv("locations_by_counts.csv", index=False)
+df.to_csv("locations_by_counts.csv", index=False) # export dataframe to csv
 
