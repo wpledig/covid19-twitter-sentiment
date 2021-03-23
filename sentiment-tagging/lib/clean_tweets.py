cd15f537fd7a33151a8ab4a26f1f37f6b230c66f
wpledig <wpl1@rice.edu>
2021-03-23 14:44:01 -0500

    Added stemmer

--- sentiment-tagging/lib/clean_tweets.py
+++ sentiment-tagging/lib/clean_tweets.py
@@ -1,4 +1,6 @@
 import re
+import nltk
+nltk.download('stopwords')
 
 
 def compose(x, lof):
@@ -40,3 +42,14 @@ def clean_tweet(tweet_text):
     rm_all = compose(tweet_text, [remove_rt, remove_tags, remove_single_chars, remove_links, remove_controls, remove_punctuation, remove_whitespace])
     lower = rm_all.lower()
     return lower
+
+
+stopword = nltk.corpus.stopwords.words('english')
+ps = nltk.PorterStemmer()
+
+
+def stem_tweet(tweet_text):
+    text_rc = re.sub('[0-9]+', '', tweet_text)
+    tokens = re.split('\W+', text_rc)
+    text = [ps.stem(word) for word in tokens if word not in stopword]
+    return ' '.join(text)
