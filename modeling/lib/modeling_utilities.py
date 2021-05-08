5024f1d3dd86ca500229d6d776913653261c8bb3
wpledig <wpl1@rice.edu>
2021-05-07 22:52:50 -0500

    Commented additional files

--- modeling/lib/modeling_utilities.py
+++ modeling/lib/modeling_utilities.py
@@ -3,20 +3,29 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
 import os
-import sys
 
 
 def get_sentiment():
+    """
+    Returns a dataframe of the daily average compound sentiment from the Twitter dataset
+    """
     return pd.read_csv(os.path.join(os.path.dirname(__file__), "../../sentiment-tagging/data/vader_compound_dailies.csv"),
                        parse_dates=['day'], infer_datetime_format=True)
 
 
 def get_emotion():
+    """
+    Returns a dataframe of the daily average emotion vector values from the Twitter dataset
+    """
     return pd.read_csv(os.path.join(os.path.dirname(__file__), "../../sentiment-tagging/data/txt2emotion_dailies.csv"),
                        parse_dates=['day'], infer_datetime_format=True)
 
 
 def get_positivity_rate():
+    """
+    Returns a dataframe of the daily positivity rate of COVID-19 tests in the US, trimmed to the dates available in the
+    Twitter dataset
+    """
     comp_df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../../sentiment-tagging/data/vader_compound_dailies.csv"),
                           parse_dates=['day'], infer_datetime_format=True)
     pos_df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../../exploration/data/positivity_rate.csv"),
@@ -30,6 +39,9 @@ def get_positivity_rate():
 
 
 def get_day_of_week():
+    """
+    Returns a numpy array of the day of the week for each day in the Twitter dataset, where 0 is Monday and 6 is Sunday
+    """
     comp_df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../../sentiment-tagging/data/vader_compound_dailies.csv"),
                           parse_dates=['day'], infer_datetime_format=True)
     days = (comp_df.index.to_numpy() - 1) % 7
@@ -37,6 +49,12 @@ def get_day_of_week():
 
 
 def scale_dataset(arr):
+    """
+    Scales the inputted array to the range [0, 1] based on the array's minimum and maximum
+    :param arr: The array to be scaled
+    :return: A tuple of the scaled array and the scaler that has been fitted to the array (useful for re-scaling data
+    back to the original data range)
+    """
     scaler = MinMaxScaler(feature_range=(0, 1))
     if len(arr.shape) == 1:
         arr = arr.reshape(-1, 1)
@@ -61,6 +79,13 @@ def set_lag(x_set, y_set, lag):
 
 
 def plot_loss(history, train_label, valid_label, title):
+    """
+    Plots a graph of training and validation loss over time for a given model
+    :param history: an object containing sequences of loss values for training and validation
+    :param train_label: the label for training loss values in the "history" object
+    :param valid_label: the label for testing loss values in the "history" object
+    :param title: string to be used as the title for the outputted graph
+    """
     plt.figure(figsize=(10, 4))
     plt.plot(history[train_label], label='Training')
     plt.plot(history[valid_label], label='Validation')
@@ -72,6 +97,15 @@ def plot_loss(history, train_label, valid_label, title):
 
 
 def plot_prediction(dates, real_data, prediction, training_ind, title):
+    """
+    Plots a graph of predictions of COVID-19 test positivity rate for a model
+    :param dates: range of dates for the predictions/real data (sequence of x values)
+    :param real_data: list of data points for actual positivity rate
+    :param prediction: list of predicted data points for positivity rate
+    :param training_ind: number representing the last index of training data in "prediction" (if set to 0, this function
+            assumes no training data is included in "prediction"
+    :param title: string for the title of the outputted graph
+    """
     fig, ax = plt.subplots(figsize=(8, 4))
     if training_ind > 0:
         ax.plot(dates[:training_ind], prediction[:training_ind], label='Training Prediction')
--- sentiment-tagging/lib/clean_tweets.py
+++ sentiment-tagging/lib/clean_tweets.py
@@ -1,9 +1,17 @@
 import re
 import nltk
+
+# Download corpus of stopwords if not existing
 nltk.download('stopwords')
 
 
 def compose(x, lof):
+    """
+    Applies a list of functions to a value by composition
+    :param x: original input value to the functions
+    :param lof: list of functions that take a single argument and return a value
+    :return: the resulting value of composing "lof" and applying that function to "x"
+    """
     res = x
     for func in lof:
         res = func(res)
@@ -11,45 +19,94 @@ def compose(x, lof):
 
 
 def remove_rt(x):
+    """
+    Removes any retweet markers from the text of a tweet
+    :param x: The text of a tweet
+    :return: "x" with retweet text removed
+    """
     return re.sub('RT @\w +: ', "", x)
 
 
 def remove_tags(x):
+    """
+    Removes any tagged usernames from the text of a tweet
+    :param x: The text of a tweet
+    :return: "x" with tagged usernames removed
+    """
     return re.sub("(@\w+)", "", x)
 
 
 def remove_single_chars(x):
+    """
+    Removes any single character words from the text of a tweet
+    :param x: The text of a tweet
+    :return: "x" with single character words removed
+    """
     return re.sub(" ([0-9A-Za-z \t]) ", " ", x)
 
 
 def remove_links(x):
+    """
+    Removes any links from the text of a tweet
+    :param x: The text of a tweet
+    :return: "x" with links removed
+    """
     return re.sub("(\w+:\/\/\S+)", "", x)
 
 
 def remove_controls(x):
+    """
+    Removes any control characters from the text of a tweet
+    :param x: The text of a tweet
+    :return: "x" with control characters removed
+    """
     return re.sub("(\\\\x\w{2})|(\\\\\w)", "", x)
 
 
 def remove_punctuation(x):
+    """
+    Removes any punctuation from the text of a tweet
+    :param x: The text of a tweet
+    :return: "x" with punctuation removed
+    """
     return re.sub("[^\w\d\s]", "", x)
 
 
 def remove_whitespace(x):
+    """
+    Removes any whitespace from the text of a tweet
+    :param x: The text of a tweet
+    :return: "x" with whitespace removed
+    """
     return re.sub("[^\w\d]+", " ", x)
 
 
 def clean_tweet(tweet_text):
+    """
+    Applies all above cleaning functions to the inputted tweet text and also sets the tweet to lowercase
+    :param tweet_text: The text of a tweet
+    :return: The fully cleaned tweet text
+    """
     rm_all = compose(tweet_text, [remove_rt, remove_tags, remove_single_chars, remove_links, remove_controls, remove_punctuation, remove_whitespace])
     lower = rm_all.lower()
     return lower
 
 
+# Download a corpus of stopwords
 stopword = nltk.corpus.stopwords.words('english')
 ps = nltk.PorterStemmer()
 
 
 def stem_tweet(tweet_text):
+    """
+    Applies stemming to an inputted tweet
+    :param tweet_text: The text of a tweet
+    :return: The fully stemmed tweet text
+    """
+    # Remove numbers
     text_rc = re.sub('[0-9]+', '', tweet_text)
+    # Split on whitespace
     tokens = re.split('\W+', text_rc)
+    # Remove stopwords and apply stemming
     text = [ps.stem(word) for word in tokens if word not in stopword]
     return ' '.join(text)
